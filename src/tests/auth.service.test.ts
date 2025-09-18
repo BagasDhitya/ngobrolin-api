a2cef54c47f23732d8e59bfa798b417ac6f8c155
BagasDhitya <bagasdhityataufiqqi98@gmail.com>
2025-09-18 16:32:09 +0700

    chore: add workflow CI for testing

--- src/tests/auth.service.test.ts
+++ src/tests/auth.service.test.ts
@@ -1,12 +1,19 @@
 import { AuthService } from '../services/auth.service'
 import { AppError } from '../helpers/response.helper'
+
 import { prisma } from '../config/prisma'
 
 // mock semua dependency
 jest.mock('../config/prisma')
 jest.mock('../helpers/cloudinary.helper')
 jest.mock('../helpers/mailer.helper')
-jest.mock('../helpers/jwt.helper')
+jest.mock('../helpers/jwt.helper', () => {
+    return {
+        JWTHelper: jest.fn().mockImplementation(() => ({
+            generateToken: jest.fn().mockReturnValue('mocked.jwt.token'),
+        }))
+    }
+})
 jest.mock('bcrypt')
 
 const authService = new AuthService()
@@ -36,5 +43,43 @@ describe('AuthService', () => {
             expect(prisma.profile.create).toHaveBeenCalled()
             expect(prisma.otpCode.create).toHaveBeenCalled()
         })
+    })
+
+    describe('login', () => {
+        it('should login successfully with valid credentials and OTP', async () => {
+            const mockUser = {
+                id: '1',
+                email: 'john.doe@example.com',
+                password: '123456',
+                role: 'USER'
+            }
+
+                ; (prisma.profile.findUnique as jest.Mock).mockResolvedValueOnce(mockUser)
+                ; (prisma.otpCode.findFirst as jest.Mock).mockResolvedValueOnce({ id: 'otp1' })
+                ; (prisma.otpCode.update as jest.Mock).mockResolvedValueOnce(true)
+
+            const result = await authService.login({
+                email: 'john.doe@example.com',
+                password: '123456',
+                otp: '123456'
+            })
+
+            expect(result).toHaveProperty('user')
+            expect(result).toHaveProperty('token', 'mocked.jwt.token')
+            expect(prisma.otpCode.update).toHaveBeenCalledWith({
+                where: { id: 'otp1' },
+                data: { used: true }
+            })
+        })
+    })
+
+    it('should throw error if user not found', async () => {
+        (prisma.profile.findUnique as jest.Mock).mockResolvedValueOnce(null)
+
+        await expect(authService.login({
+            email: 'wrong@example.com',
+            password: '123456',
+            otp: '123456'
+        })).rejects.toThrow(AppError)
     })
 })
